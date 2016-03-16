6f9cf4904b7da36960e81056022c73a75dfce23d
Jean-Baptiste <contact@jbpasquier.eu>
2016-03-16 18:05:42 +0100

    debbug

--- public/js/controllers/userCreationController.js
+++ public/js/controllers/userCreationController.js
@@ -1,15 +1,18 @@
 // MAIN CONTROLLER
 function userCreationController($scope, $http, userService, $location) {
     $scope.datas = userService.datas;
-    if(!$scope.datas.current) $scope.datas.current = 1;
-    if(!$scope.datas.currentGender) $scope.datas.currentGender = 2;
+    $scope.isValidPassword = function(regex) { //isValidPassword('[A-Z]+')
+        return $scope.datas.password ? !(new RegExp('^.*'+regex+'.*$').test($scope.datas.password)) : false;
+    };
+    if (!$scope.datas.current) $scope.datas.current = 1;
+    if (!$scope.datas.currentGender) $scope.datas.currentGender = 2;
     $scope.changeGender = function(gender) {
         $scope.datas.currentGender = gender;
     };
     $scope.step = function(where) {
-        var path = '/userCreation/step-'+where;
-        if(where == 'next') {
-            path = '/userCreation/step-'+($scope.datas.current+1);
+        var path = '/userCreation/step-' + where;
+        if (where == 'next') {
+            path = '/userCreation/step-' + ($scope.datas.current + 1);
         }
         userService.datas = $scope.datas;
         $location.path(path);
