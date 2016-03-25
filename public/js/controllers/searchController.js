ce979b8ad6c45bd3426aeeddfe941f9c406659fe
Jean-Baptiste <contact@jbpasquier.eu>
2016-03-25 11:34:59 +0100

    Set Yoan as first user

--- public/js/controllers/searchController.js
+++ public/js/controllers/searchController.js
@@ -1,6 +1,6 @@
 // SEARCH CONTROLLER
 function searchController($scope, $location, $http, userFactory, searchFactory, userService) {
-    userService.getUserById('56f2d9b261344f38095e5db8').then(function (e) {
+    userService.getUserById('56f2c1f74d31b95c5033c677').then(function (e) {
         $('body').css('background-image', 'none').css('background-color', 'black');
         userFactory.datas = e.data[0];
         $scope.username = userFactory.datas.profile.firstname + ' ' + userFactory.datas.profile.lastname;
