ef1c0c89aa9a78e18c1b5062caa1cdfe12d9ca30
IbanGF <ueqiban@gmail.com>
2016-03-28 18:17:36 +0200

    chat added

--- public/js/controllers/meetController.js
+++ public/js/controllers/meetController.js
@@ -1,7 +1,7 @@
 // MEET CONTROLLER
 function meetController($scope, $location, $http, $routeParams, userFactory, searchFactory, userService) {
     var id = $routeParams.id;
-    userService.getUserById('56f2c1f74d31b95c5033c677').then(function (e) {
+    userService.getUserById('56f6a4ad4cb92db115699b9c').then(function (e) {
         $('body').css('background-image', 'none').css('background-color', 'black');
         userFactory.datas = e.data[0];
         $scope.username = userFactory.datas.profile.firstname + ' ' + userFactory.datas.profile.lastname;
@@ -10,6 +10,15 @@ function meetController($scope, $location, $http, $routeParams, userFactory, sea
             $scope.selectedUser = f.data[0];
             console.log(id);
             console.log(f.data[0]);
+        });
+        var socket = io();
+        $('.chat').submit(function () {
+            socket.emit('chat message', $('#m').val());
+            $('#m').val('');
+            return false;
+        });
+        socket.on('chat message', function (msg) {
+            $('#messages').prepend($('<li>').html('<span class="nickname">' + $scope.username + '</span> : ' + msg + '<br/><span class="pull-right">' + ((new Date()).toLocaleTimeString()) + '</span>'));
         });
     });
 }
