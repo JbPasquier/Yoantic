c564b36fd6493c5714d384917a13a19b95e23f01
IbanGF <ueqiban@gmail.com>
2016-03-29 12:22:09 +0200

    base 64 correction profile update

--- public/js/controllers/profileController.js
+++ public/js/controllers/profileController.js
@@ -1,16 +1,16 @@
 // PROFILE CONTROLLER
 function profileController($scope, $location, $http, userFactory, userService) {
-    userService.getUserById(userFactory.datas._id).then(function(e) { //'56f6a4ad4cb92db115699b9c'
+    userService.getUserById(userFactory.datas._id).then(function (e) { //'56f6a4ad4cb92db115699b9c'
         $('body').css('background-image', 'none').css('background-image', 'url("./assets/bg.jpg")');
         userFactory.datas = e.data[0];
+        userFactory.datas.avatar = [];
         $scope.username = userFactory.datas.profile.firstname + ' ' + userFactory.datas.profile.lastname;
         $scope.user = userFactory.datas;
-        $scope.toBase64 = function(files) {
+        $scope.toBase64 = function (files) {
             userService.toBase64(files);
-            $scope.user.avatar = userFactory.datas.avatar;
+            $scope.user.profile.avatar = userFactory.datas.avatar;
         };
-
-        $scope.sendProfile = function() {
+        $scope.sendProfile = function () {
             userService.updateAccount($scope.user._id, $scope.user.profile, $scope.user.search);
         };
     });
